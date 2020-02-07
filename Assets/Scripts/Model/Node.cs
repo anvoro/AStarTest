4f28fe5ae5b5f1af9352911f93dc330e83845cf1
Anton Vorobev <naxxor@mail.ru>
2020-02-07 09:27:30 +0300

    initial

---
+++ Assets/Scripts/Model/Node.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Assets.Scripts.Model
+{
+    public class Node
+    {
+        private bool _isRoad;
+
+        public Node Parent { get; set; }
+
+        public float GCost { get; set; }
+        public float HCost { get; set; }
+        public float FCost => this.GCost + this.HCost;
+
+        public bool IsWall { get; set; }
+        public bool IsStart { get; set; }
+        public bool IsFinish { get; set; }
+
+        public bool IsRoad
+        {
+            get => this._isRoad;
+            set
+            {
+                this._isRoad = value;
+                this.OnSetRoad?.Invoke();
+            }
+        }
+
+        public int PositionX { get; }
+        public int PositionY { get; }
+
+        public int Id { get; }
+
+        public event Action OnSetRoad;
+
+        public Node(int id, int positionX, int positionY)
+        {
+            this.Id = id;
+
+            this.PositionX = positionX;
+            this.PositionY = positionY;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Node otherNode = obj as Node;
+            if (otherNode == null)
+                return false;
+
+            return this.Id == otherNode.Id;
+        }
+
+        protected bool Equals(Node other)
+        {
+            return this.Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
+
+        public int CompareTo(Node other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+
+            return this.FCost.CompareTo(other.FCost);
+        }
+
+        public override string ToString()
+        {
+            return this.FCost.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/View/DrawModeSelector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.View
+{
+    public enum DrawMode
+    {
+        Walls = 0,
+
+        Start = 1,
+
+        Finish = 2
+    }
+
+    public class DrawModeSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private ToggleGroup _drawModeToggles;
+
+        [SerializeField]
+        private Toggle[] _toggles;
+
+        public static DrawModeSelector Instance { get; private set; }
+
+        private void Start()
+        {
+            Instance = this;
+        }
+
+        public DrawMode GetCurrentDrawMode()
+        {
+            switch (Array.IndexOf(this._toggles, this._drawModeToggles.ActiveToggles().First()))
+            {
+                case 0:
+                    return DrawMode.Walls;
+
+                case 1:
+                    return DrawMode.Start;
+
+                case 2:
+                    return DrawMode.Finish;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
